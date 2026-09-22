b57811f3599f8164d3316e757b54df8fcb11b67e
Danut Radu <d_dany_radu@yahoo.com>
2026-09-22 18:07:10 +0300

    Initialize multi-module Maven build

---
+++ services/restaurant-ops-service/src/main/java/com/food/delivery/ops/RestaurantOpsApplication.java
@@ -0,0 +1,11 @@
+package com.food.delivery.ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantOpsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantOpsApplication.class, args);
+    }
+}
